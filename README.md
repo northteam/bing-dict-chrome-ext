d802faa52d1b52b4a9a698076f2118672f890ab3
U-INTERNAL\liyufang <northteam@gmail.com>
2013-06-17 03:16:32 +0800

    a extension for google chrome.

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ bing-dict-chrome-ext
 ====================
 
 A chrome extension to search words using Bing dict.
+
+
+Download the extension here:
+*https://chrome.google.com/webstore/detail/bing-dict/kjcijldicnphljpiildbemhlccgnpkch .*
+
---
+++ background.js
@@ -0,0 +1,15 @@
+
+
+function openQueryTab(info, tab)
+{
+	var query_url = "http://cn.bing.com/dict/search?q=" + info.selectionText;
+	chrome.tabs.create({"url":query_url, "active":true});
+}
+
+
+var title = chrome.i18n.getMessage("contextTitle");
+var ID = chrome.contextMenus.create({"title": title,
+		"contexts": ["selection"],
+		"onclick": openQueryTab});
+
+
