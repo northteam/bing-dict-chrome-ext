8c92a26ddd9aedbdbdf7d82af4523e22a2e41be3
northteam <northteam@gmail.com>
2013-07-01 23:53:45 +0800

    set openerTabId so that the original tab would be activated after closing the newly created one.

--- src/background.js
+++ src/background.js
@@ -3,14 +3,20 @@
 function openQueryTab(info, tab)
 {
 	var query_url = "http://cn.bing.com/dict/search?q=" + encodeURIComponent(info.selectionText);
-	var tab_index = tab.index + 1;
-	chrome.tabs.create({"url":query_url, "index":tab_index, "active":true});
+	chrome.tabs.create({
+		"url": query_url,
+		"index": tab.index + 1,
+		"openerTabId": tab.id,
+		"active":true
+	});
 }
 
 
 var title = chrome.i18n.getMessage("contextTitle");
-var ID = chrome.contextMenus.create({"title": title,
+var ID = chrome.contextMenus.create({
+		"title": title,
 		"contexts": ["selection"],
-		"onclick": openQueryTab});
+		"onclick": openQueryTab
+	});
 
 
