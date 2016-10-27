a8739925a5cbbf2ec0713c0babb90ca6c84726d4
northteam <northteam@gmail.com>
2016-10-27 21:01:39 +0800

    fix empty result issue

--- src/background.js
+++ src/background.js
@@ -2,7 +2,7 @@
 
 function openQueryTab(info, tab)
 {
-	var query_url = "http://cn.bing.com/dict/search?q=" + encodeURIComponent(info.selectionText);
+	var query_url = "http://cn.bing.com/dict/search?mkt=zh-CN&q=" + encodeURIComponent(info.selectionText);
 	chrome.tabs.create({
 		"url": query_url,
 		"index": tab.index + 1,
